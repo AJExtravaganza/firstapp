08540de6614b15a232ad052e7534179e660baf7b
Alexander Dunn <alexdunnmobile@gmail.com>
2020-02-16 17:59:50 -0800

    Removed not-currently-required modules

--- lib/teasessions/brew_profile_info.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-import '../models.dart';
-
-//currently defined in teasessions.dart
--- lib/teasessions/steep_counter.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-//currently defined in teasessions.dart
-
