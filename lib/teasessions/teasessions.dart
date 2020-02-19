54515da548a506400f5e98bf0c6bacd55e5cdbe3
Alexander Dunn <alexdunnmobile@gmail.com>
2020-02-18 19:04:28 -0800

    Added proper icons for brew parameters, minor styling

--- lib/teasessions/teasessions.dart
+++ lib/teasessions/teasessions.dart
@@ -5,6 +5,8 @@ import 'package:firstapp/models/tea.dart';
 import 'package:firstapp/teasessions/steep_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/fa_icon.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 class SessionsView extends StatefulWidget {
@@ -56,7 +58,7 @@ class BrewProfileInfo extends StatelessWidget {
             flex: 3,
             child: Container(),
           ),
-          Expanded(flex: 3, child: TeaNameRow()),
+          Expanded(flex: 6, child: TeaNameRow()),
           Expanded(flex: 3, child: BrewingParametersRow()),
           Expanded(
             flex: 3,
@@ -90,7 +92,7 @@ class TeaNameRow extends StatelessWidget {
                 child: Text(
           currentTea.asString(),
           textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20),
+          style: TextStyle(fontSize: 30),
         )))
       ],
     );
@@ -100,34 +102,48 @@ class TeaNameRow extends StatelessWidget {
 class BrewingParametersRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    BrewingVessel currentBrewingVessel = Provider.of<ActiveTeaSessionModel>(context).brewingVessel;
-    BrewProfile currentBrewProfile = Provider.of<ActiveTeaSessionModel>(context).brewProfile;
+    BrewingVessel currentBrewingVessel =
+        Provider.of<ActiveTeaSessionModel>(context).brewingVessel;
+    BrewProfile currentBrewProfile =
+        Provider.of<ActiveTeaSessionModel>(context).brewProfile;
     return Row(children: <Widget>[
       Expanded(
-        flex: 2,
+        flex: 1,
         child: Container(),
       ),
       Expanded(
-        flex: 2,
-        child: BrewingParameterRowElement(Icons.blur_circular,
+        flex: 4,
+        child: BrewingParameterRowElement(FontAwesomeIcons.leaf,
             '${currentBrewProfile.getDose(currentBrewingVessel).toStringAsFixed(1)}g'),
       ),
       Expanded(
-          flex: 2,
-          child: BrewingParameterRowElement(
-              Icons.blur_off, '1:${currentBrewProfile.nominalRatio}')),
-      Expanded(
-        flex: 2,
-        child: BrewingParameterRowElement(
-            Icons.network_wifi, '${currentBrewingVessel.volumeMilliliters}ml'),
+        flex: 1,
+        child: Container(),
       ),
       Expanded(
-        flex: 2,
-        child: BrewingParameterRowElement(
-            Icons.ac_unit, '${currentBrewProfile.brewTemperature}°C'),
+          flex: 4,
+          child: BrewingParameterRowElement(FontAwesomeIcons.balanceScale,
+              '1:${currentBrewProfile.nominalRatio}')),
+      Expanded(
+        flex: 1,
+        child: Container(),
       ),
       Expanded(
-        flex: 2,
+        flex: 4,
+        child: BrewingParameterRowElement(FontAwesomeIcons.tint,
+            '${currentBrewingVessel.volumeMilliliters}ml'),
+      ),
+      Expanded(
+        flex: 1,
+        child: Container(),
+      ),
+      Expanded(
+        flex: 4,
+        child: BrewingParameterRowElement(FontAwesomeIcons.temperatureHigh,
+            '${currentBrewProfile.brewTemperature}°C'),
+      ),
+      Expanded(
+        flex: 1,
         child: Container(),
       ),
     ]);
@@ -143,7 +159,13 @@ class BrewingParameterRowElement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: <Widget>[Icon(this.icon), Text(this.valueText)],
+      children: <Widget>[
+        FaIcon(this.icon),
+        Text(
+          this.valueText,
+          style: TextStyle(fontSize: 18),
+        )
+      ],
     );
   }
 }
